c02507f97fb11fefd1bf2b0e8897c46e430a8785
An Kieu <134972544+KoisenAn@users.noreply.github.com>
2026-03-14 14:53:42 -0400

    Finished UI Handler

---
+++ Day After Day/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,88 @@
+using System;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+    public TextMeshProUGUI  timeText;
+
+    public TextMeshProUGUI  moneyText; 
+
+    public GameObject proteinBar;
+
+    public GameObject vegetableBar;
+
+    public GameObject carbsBar;
+
+    public GameObject fruitBar;
+
+    private Color defaultGray;
+
+    private Color proteinRed;
+
+    private Color vegetableGreen;
+
+    private Color yellowGrain;
+
+    private Color pinkFruit;
+
+    void Awake()
+    {
+        defaultGray = new Color(204f/255f, 204f/255f, 204f/255f, 1f);
+        proteinRed = new Color(255f/255f, 81f/255f, 81f/255f, 1f);
+        vegetableGreen = new Color(73f/255f, 210f/255f, 86f/255f, 1f);
+        yellowGrain = new Color(255f/255f, 231f/255f, 50f/255f, 1f);
+        pinkFruit = new Color(255f/255f, 179f/255f, 186f/255f, 1f);
+
+        UpdateBar(proteinBar, 7, defaultGray);
+        UpdateBar(vegetableBar, 7, defaultGray);
+        UpdateBar(carbsBar, 7, defaultGray);
+        UpdateBar(fruitBar, 7, defaultGray);
+
+        UpdateBar(proteinBar, 2, proteinRed);
+        UpdateBar(vegetableBar, 3, vegetableGreen);
+        UpdateBar(carbsBar, 0, yellowGrain);
+        UpdateBar(fruitBar, 7, pinkFruit);
+
+        UpdateTime(490);
+        UpdateMoney(2130);
+    }
+
+
+    void UpdateBar(GameObject bar, int filled, Color color) 
+    {
+        Transform transform = bar.GetComponent<Transform>();
+
+        for (int i = 0; i < Math.Clamp(filled, 0, 7); i++)
+        {
+            Transform child = transform.GetChild(i);
+
+            Image img = child.GetComponent<Image>();
+            if (img != null)
+            {
+                img.color = color;
+            }
+        }
+    }
+
+    public void UpdateTime(int timeInMinutes)
+    {
+        int hours = timeInMinutes / 60;
+        int minutes = timeInMinutes % 60;
+
+        string period = hours >= 12 ? "PM" : "AM";
+
+        int displayHour = hours % 12;
+        if (displayHour == 0)
+            displayHour = 12;
+
+        timeText.text = $"{displayHour}:{minutes:00} {period}";
+    }
+
+    public void UpdateMoney(int money) 
+    {
+        moneyText.text = "$  " + money;
+    }
+}

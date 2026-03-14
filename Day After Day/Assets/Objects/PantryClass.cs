757355aa841a87d38b76d2a31c66c8878a96f530
MasonBartosiak <mbartosi@purdue.edu>
2026-03-14 13:52:17 -0400

    Finished player movement system

--- Day After Day/Assets/Objects/PantryClass.cs
+++ Day After Day/Assets/Objects/PantryClass.cs
@@ -1,31 +1,66 @@
 using UnityEngine;
+using System;
 
 public class PantryClass : MonoBehaviour
 {
-    public int foodQuantity;
-    public Vector2 playerPosition; 
-    
+   
+    public int[] food;
+    public PantryClass[] closestNodes;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var pos = transform.position;
-        foodQuantity = Random.Range(10, 100);
-        //Position = new Vector2(pos.x, pos.y);
-    }
+        var allNodes = FindObjectsByType<PantryClass>(FindObjectsSortMode.None);
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        closestNodes = new PantryClass[4];
+        for (int i = 0; i < closestNodes.Length; i++)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-
-            if (hit.collider != null) {
-                var positions = hit.collider.gameObject.transform.position;
-                Debug.Log ("CLICKED " + hit.collider.name + " At " + new Vector2(positions.x, positions.y));
-                //int pantryFood = hit.collider.gameObject.GetComponent<Script>()
+            closestNodes[i] = getClosestNode(allNodes);
+        }
+        food = new int[5];
+        if (tag == "Node")
+        {
+            // Any value or functionality that should be unique to Nodes specifically
+        } 
+        else
+        {
+            // Not a node and therefore a pantry
+            for (int i = 0; i < food.Length; i++)
+            {
+            food[i] = UnityEngine.Random.Range(0, 10);
             }
         }
+    
     }
+
+    // Helper method to return the closest node in an array of all nodes on the map
+    PantryClass getClosestNode (PantryClass[] nodes)
+    {
+        PantryClass tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector2 currentPos = transform.position;
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            if (nodes[i] == this) continue;
+            float dist = Vector2.Distance(nodes[i].transform.position, currentPos);
+            if (dist < minDist)
+            {   
+                bool SkipFlag = false;
+                for (int j = 0; j < closestNodes.Length; j++)
+                {
+                    if (nodes[i] == closestNodes[j])
+                    {
+                       SkipFlag = true; 
+                       break;
+                    }
+                }
+                if (!SkipFlag)
+                {
+                     tMin = nodes[i];
+                    minDist = dist;
+                }
+            }
+        }
+        return tMin;
+    } // getClosestNode
 }
